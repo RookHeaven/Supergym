963402eea9b67d6ded88c691f5a057b59614086a
Roman Grachev <romagra92@gmail.com>
2023-05-17 17:07:12 +0500

    Внес исправления в блок с видео

--- source/js/modules/video/init-video.js
+++ source/js/modules/video/init-video.js
@@ -1,13 +1,23 @@
+const videoContainer = document.querySelector('.video');
+const videoPicture = videoContainer.querySelector('picture');
 const videoBtn = document.querySelector('.video__button');
-const iframe = document.querySelector('.video__iframe');
 
 const onPlayClick = () => {
-  if (videoBtn && iframe) {
-    videoBtn.addEventListener('click', () => {
-      videoBtn.style.display = 'none';
-      iframe.style.display = 'block';
-    });
-  }
+  const video = document.createElement('iframe');
+
+  video.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
+  video.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+  video.setAttribute('allowfullscreen', '');
+  video.classList.add('video__iframe');
+  videoContainer.append(video);
 };
+
+if (videoContainer && videoPicture && videoBtn) {
+  videoBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    videoPicture.style.display = 'none';
+    videoBtn.style.display = 'none';
+  });
+}
 
 export {onPlayClick};
